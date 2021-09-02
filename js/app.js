ff4d4a59fd2a894eee69eb7ccf60772a16e012e9
rased100 <rased100@gmail.com>
2021-09-02 12:16:53 +0600

    second-commit

--- js/app.js
+++ js/app.js
@@ -10,9 +10,36 @@ const searchBook = () => {
     fetch(url)
         .then(res => res.json())
         .then(data => showSearchBooks(data.docs));
+    // .then(data => showResultsValue(data.docs));
 }
+// const showResultsValue = value => {
+//     console.log(value.length);
+// }
 
 const showSearchBooks = books => {
+    console.log(books.length);
+    if (books.length === 0) {
+        // No result found
+        const totalSearchResult = document.getElementById('total-srarch-result');
+        const div1 = document.createElement('div');
+        div1.innerHTML = `
+            <h1 class="text-center">No Resut Found</h1>
+`;
+        totalSearchResult.appendChild(div1);
+    }
+    else {
+        // total results in number
+        const totalSearchResult = document.getElementById('total-srarch-result');
+        const div1 = document.createElement('div');
+        div1.innerHTML = `
+    <p class="text-center  ">About <span class="fw-bold" >${books.length}</span> Search Results</p><hr class="w-50 mx-auto">
+    `;
+        totalSearchResult.appendChild(div1);
+    }
+
+
+
+    // book results
     const searchBookResult = document.getElementById('search-result');
     books.forEach(book => {
         console.log(book);
@@ -24,9 +51,9 @@ const showSearchBooks = books => {
                     <h5 class="card-title text-center">${book.title}</h5>
                 </div>
                 <div class="card-body">
-                    <p class="card-text">Author: ${book.author_name}</p>
-                    <p class="card-text">Publish-Year: ${book.first_publish_year}</p>
-                    <p class="card-text">Publisher: ${book.publisher}</p>
+                    <p class="card-text"><span class="fw-bold">Author:</span> ${book.author_name}</p>
+                    <p class="card-text"><span class="fw-bold">Publish-Year:</span> ${book.first_publish_year}</p>
+                    <p class="card-text"><span class="fw-bold">Publisher:</span> ${book.publisher}</p>
                 </div>
             </div>
         `;
